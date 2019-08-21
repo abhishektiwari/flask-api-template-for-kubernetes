47644370fe36da75a82e7c02899abe95d26bfc56
Abhishek Tiwari <abhishek.twr@gmail.com>
2019-08-21 10:17:51 +1000

    Minor change

--- {{cookiecutter.project_slug}}/app/apis/options.py
+++ {{cookiecutter.project_slug}}/app/apis/options.py
@@ -2,7 +2,7 @@
 Path prefix and CORS options for service
 """
 
-PATH_PREFIX = "/test"
+PATH_PREFIX = "/{{ cookiecutter.api_prefix }}"
 
 ERR_MISSIN = 'Missing value or values.'
 ERR_UNAUTH = 'Unauthorised operation.'
